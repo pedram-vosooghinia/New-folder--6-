791b20a6dcd3dd05762c9e24200459add90e3680
Pedram <p.vosooghinia69@gmail.com>
2023-11-28 21:55:01 +0330

    delete-material-tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import "../styles/globals.css";
-import { ThemeProvider } from "@material-tailwind/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <div >
-      <ThemeProvider>
           <Component {...pageProps} />
-      </ThemeProvider>
     </div>
   );
 }
