7a5cee50ac51ca8cb274f846c5537a64d21b7e3a
Pedram <p.vosooghinia69@gmail.com>
2023-11-28 21:44:21 +0330

    fix-tetisan-link-footer

--- components/Main/Footer.js
+++ components/Main/Footer.js
@@ -15,13 +15,12 @@ const Footer = () => {
   return (
     <footer className="bg-pedram-4 relative w-full text-pedram-2">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="text-pedram-1 m-6 text-4xl font-bold">TETISAN</div>
+        <div className="text-pedram-1 m-6 text-4xl font-bold">
+          <Link href="/">TETISAN </Link>
+        </div>
         <div className="flex md:flex-row  p-6 rtl w-full ">
           {links.map((category) => (
-            <div
-              key={category.title}
-              className="text-lg p-6"
-            >
+            <div key={category.title} className="text-lg p-6">
               <div className="text-pedram-1 font-bold">{category.title}</div>
               <ul>
                 {category.items.map((item) => (
