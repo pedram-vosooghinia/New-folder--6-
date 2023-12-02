1a773b3550bd5cf27870245eddaa0666b315739d
Pedram <p.vosooghinia69@gmail.com>
2023-12-02 21:36:44 +0330

    add-api-product

--- components/dashboard/ProductForm.js
+++ components/dashboard/ProductForm.js
@@ -1,20 +1,20 @@
 // components/AddProductForm.js
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import axios from "axios";
-import { useState } from 'react';
+import { useState } from "react";
 
 const AddProductForm = () => {
-  const handleSubmit = async (values, { setSubmitting }) => {
-    try {
-      const response = await axios.post("/api/products", values);
-      console.log("Product added:", response.data);
-      // Optionally, you can redirect the user or perform other actions after adding the product
-    } catch (error) {
-      console.error("Error adding product:", error);
-    } finally {
-      setSubmitting(false);
-    }
-  };
+    const handleSubmit = async (values, { setSubmitting }) => {
+        try {
+          const response = await axios.post('/api/products', values);
+          console.log('Product added:', response.data);
+          // Optionally, you can redirect the user or perform other actions after adding the product
+        } catch (error) {
+          console.error('Error adding product:', error);
+        } finally {
+          setSubmitting(false);
+        }
+      };
   const [imagePreview, setImagePreview] = useState(null);
 
   const handleImageChange = (event) => {
@@ -27,13 +27,14 @@ const AddProductForm = () => {
     }
   };
   const inputItems = [
-    { id: "productName", label: "نام محصول", type: "text", className: "block-inline" },
-    { id: "price", label: "قیمت", type: "number", className: "inline" },
-    { id: "inventory", label: "موجودی", type: "number", className: "block-inline" },
-    { id: "barcode", label: "کد محصول", type: "number", className: "block-inline" },
-    { id: "numberInPack", label: "تعداد در بسته", type: "number", className: "block-inline" },
-    { id: "coloring", label: "تعداد رنگ بندی", type: "number", className: "block-inline" },
-    { id: "image", label: "تصویر", type: "file", className: "block-inline" },
+    { id: "productName", label: "نام محصول", type: "text" },
+    { id: "price", label: "قیمت", type: "number" },
+    { id: "inventory", label: "موجودی", type: "number" },
+    { id: "barcode", label: "کد محصول", type: "number" },
+    { id: "numberInPack", label: "تعداد در بسته", type: "number" },
+    { id: "coloring", label: "تعداد رنگ بندی", type: "number" },
+    { id: "size", label: "ابعاد", type: "text" },
+    { id: "image", label: "تصویر", type: "file" },
   ];
 
   return (
@@ -45,7 +46,6 @@ const AddProductForm = () => {
         barcode: "",
         numberInPack: "",
         coloring: 0,
-        
       }}
       validate={(values) => {
         // Implement custom validation if needed
@@ -55,55 +55,57 @@ const AddProductForm = () => {
       onSubmit={handleSubmit}
     >
       <Form className="rtl max-w-lg mx-auto mt-8 p-6 bg-white rounded shadow-lg">
-      
         <div className="flex justify-between flex-wrap">
-        {inputItems.map((item, index) => (
-        <div className={`mb-4 ${index === 0 ? 'w-full' : ' w-1/2'} px-2 ${item.className}`} key={index}>
-        <label
-              htmlFor={item.id}
-              className="block text-gray-700 text-sm font-bold mb-2"
+          {inputItems.map((item, index) => (
+            <div
+              className={`mb-4 ${index === 0 ? "w-full" : " w-1/2"} px-2 `}
+              key={index}
             >
-              {item.label}:
-            </label>
-            {item.type === 'file' ? (
-              <div className="block-inline">
-                <input
-                  type="file"
+              <label
+                htmlFor={item.id}
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                {item.label}:
+              </label>
+              {item.type === "file" ? (
+                <div className="block-inline">
+                  <input
+                    type="file"
+                    id={item.id}
+                    name={item.id}
+                    accept="image/*"
+                    className="hidden"
+                    onChange={handleImageChange}
+                  />
+                  <label
+                    htmlFor={item.id}
+                    className="cursor-pointer w-full py-2 border rounded-md bg-blue-500 text-white text-center"
+                  >
+                    انتخاب تصویر
+                  </label>
+                  {imagePreview && (
+                    <img
+                      src={imagePreview}
+                      alt="Selected"
+                      className="mt-2 max-w-full h-auto"
+                    />
+                  )}
+                </div>
+              ) : (
+                <Field
+                  type={item.type}
                   id={item.id}
                   name={item.id}
-                  accept="image/*"
-                  className="hidden"
-                  onChange={handleImageChange}
+                  className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
                 />
-                <label
-                  htmlFor={item.id}
-                  className="cursor-pointer w-full py-2 border rounded-md bg-blue-500 text-white text-center"
-                >
-                  انتخاب تصویر
-                </label>
-                {imagePreview && (
-                  <img
-                    src={imagePreview}
-                    alt="Selected"
-                    className="mt-2 max-w-full h-auto"
-                  />
-                )}
-              </div>
-            ) : (
-              <Field
-                type={item.type}
-                id={item.id}
+              )}
+              <ErrorMessage
                 name={item.id}
-                className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+                component="div"
+                className="text-red-500 text-sm mt-2"
               />
-            )}
-            <ErrorMessage
-              name={item.id}
-              component="div"
-              className="text-red-500 text-sm mt-2"
-            />
-          </div>
-      ))}
+            </div>
+          ))}
         </div>
 
         <div className="flex justify-between">
@@ -176,9 +178,6 @@ const AddProductForm = () => {
             className="text-red-500 text-sm mt-2"
           />
         </div>
-
-
-
 
         <div className="mt-6">
           <button
