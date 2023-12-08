a6497f234219536700d03c081cad2d2da7fb5fa9
Pedram <p.vosooghinia69@gmail.com>
2023-12-08 20:44:39 +0330

    delete material tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = withMT({
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         pedram: {
-          1 : "#002642",
+          1: "#002642",
           2: "#840032",
           3: "#e59500",
           4: "#efdada",
           5: "#02040f",
-     
         },
       },
     },
-    
   },
   plugins: [],
-}
-)
+};
